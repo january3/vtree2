9f0808530c828ea1a4e6a81a4836cdd0c87c281e
January Weiner <january.weiner@gmail.com>
2026-07-22 13:53:18 +0200

    moved vtree helpers into another file

---
+++ R/vtree_helpers.R
@@ -0,0 +1,125 @@
+## helper functions for constructing vtrees
+
+
+# converts a data frame to a pattern data frame, one line per each pattern
+# / path through the tree.
+vtree_pat <- function(data, cnms, vp = TRUE) {
+  # enquos the columns so we can play with them
+  nc <- length(cnms)
+
+  # we only need the columns mentioned
+
+  for(i in 1:nc) {
+    nm <- cnms[i]
+
+    if(vp) {
+      data <- data |>
+        mutate(.denom = sum(!is.na(.data[[nm]])))
+    } else {
+      data <- data |>
+        mutate(.denom = length(.data[[nm]]))
+    }
+
+    data <- data |>
+      # denom is the denominator: the number of non-NA values for the given
+      # column.
+      # we rely here on the fact that the data is already grouped by the
+      # previous columns! e.g. in the Titanic example, when var is Sex, the
+      # data is already grouped by Class.
+      group_by(across(cnms[1:i])) |>
+      mutate(!!paste0(nm, "_n") := n()) |>
+      mutate(!!paste0(nm, "_frac") := n() / .data[[".denom"]])
+  }
+
+  # selected columns
+  selcnms <- map(cnms, ~ c(.x, paste0(.x, "_n"), paste0(.x, "_frac"))) |> unlist()
+
+  # we are not interested in individual data points, only in the summaries
+  data <- data |> 
+    summarize(across(starts_with(cnms), first),
+              .groups = "drop_last") |>
+    ungroup() |>
+    select(all_of(selcnms))
+
+  data
+}
+
+
+# this converts the data frame returned by pat2nodes to an edge data frame
+node2edge <- function(df) {
+
+  df |>
+    select(all_of(c("ID", "parent"))) |>
+    filter(!is.na(.data[["parent"]])) |>
+    rename(from = "parent", to = "ID")
+}
+
+# this one creates a node data frame directly from the pattern,
+# one line per node. It also collects the order of the nodes.
+pat2nodes <- function(pattern, columns) {
+
+  # map over the levels. i denotes the level; the node is defined by the
+  # columns 1:i.
+  ret <- map_dfr(seq_along(columns), \(i) {
+    df <- pattern |>
+
+      # select one representative row for each node
+      distinct(pick(columns[1:i]), .keep_all = TRUE) |>
+
+      # ignore columns below the current level
+      select(all_of(c(columns[1:i], 
+                      paste0(columns[i], "_n"), 
+                      paste0(columns[i], "_frac")))) |>
+      
+      # n instead of colname_n etc.
+      rename(n = paste0(columns[i], "_n"),
+             freq = paste0(columns[i], "_frac")) |>
+      mutate(level = i) |>
+
+      # this complex bit constructs the parent and ID columns. The parent
+      # is constructed from the previous columns, and the ID is constructed
+      # from all columns up to the current one. The ID consist of column
+      # name:value pairs separated by slashes.
+      # the c_across is specifically for rowwise operations
+      rowwise() |>
+      mutate(parent = ifelse(i == 1, "__ALL__:__ALL__", 
+        paste0(paste0(columns[1:(i-1)], ":", 
+                      c_across(all_of(columns[1:(i-1)]))), collapse = "/"))) |>
+      mutate(ID = paste0(paste0(columns[1:i], ":", 
+                      c_across(all_of(columns[1:i]))), collapse = "/")) |>
+
+      # we want also to store the path as a list column for easier
+      # processing downstream
+      mutate(path = list(as.list(pick(all_of(columns[1:i]))))) |>
+
+
+      ungroup() |>
+
+      # rather than keeping all the columns, we only keep
+      # node_col, which holds the column name, and node_val, which holds the 
+      # value of that column for the given node.
+      mutate(node_col = columns[i]) |>
+      mutate(node_val = .data[[columns[i]]]) |>
+
+      select(all_of(c("ID", "node_col", "node_val", "parent", 
+                      "path", "level", "n", "freq")))
+  })
+
+  N <- sum(ret |> filter(.data[["level"]] == 1) |> pull("n"))
+
+  # that special root node
+  ret <- bind_rows(tibble(ID = "__ALL__:__ALL__", 
+                               node_col = "__ALL__",
+                               node_val = "__ALL__",
+                               parent = NA_character_, 
+                               path = list(NA),
+                               level = 0, 
+                               n = N,
+                               freq = 1), ret)
+  ret <- ret |>
+    mutate(node_cv = paste0(.data[["node_col"]], ":", 
+                            .data[["node_val"]])) |>
+    select(all_of(c("ID", "node_col", "node_val", "node_cv", "parent",
+                    "path", "level", "n", "freq")))
+  ret
+}
