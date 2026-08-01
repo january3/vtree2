846ee5c2ccc548371ec82e11b6bb3a185b9566c8
January Weiner <january.weiner@gmail.com>
2026-08-01 16:50:50 +0200

    added basic support for inline grobs

--- R/grob.R
+++ R/grob.R
@@ -6,6 +6,7 @@ lwd_npc <- function(frac) {
     convertHeight(unit(frac, "npc"), "points", valueOnly = TRUE)
   )
 }
+
 # get widths from a list of grobs
 .get_widths <- function(grobs) {
     purrr::map_dbl(grobs, \(g)
@@ -146,7 +147,42 @@ adjust_fontsize <- function(x, path, font="fixed",
   x
 }
 
+
+#' Hook for vtree plots
+#'
+#' This function is called whenever a vtree_plot is plotted on a device.
+#' It's purpose is to fit the
+#' labels text into the allocated node space.
+#'
+#' @param x A vtree_plot object
+#' @importFrom grid gTree gList setChildren makeContent
+#' @importFrom grid gPath
+#' @return A gTree object with the labels adjusted to fit into the allocated space.
+#' @export
+makeContent.vtree_plot <- function(x) {
+  spec <- x$params$spec_fontsize
+
+  for(i in seq_along(spec)) {
+    s <- spec[[i]]
+    x <- adjust_fontsize(x, s$path, font=s$fs,
+                         padding = s$padding,
+                         widths = s$widths,
+                         heights = s$heights)
+  }
+
+  spec_lwd <- x$params$spec_lwd
+  lwd <- lwd_npc(0.001 * x$params$lwd)
+  for(i in seq_along(spec_lwd)) {
+    s <- spec_lwd[[i]]
+    x <- adjust_linewidth(x, s$path,
+                          lwd = x$params$lwd, s$nokids %||% FALSE)
+  }
+
+  x
+}
+
 # create grobs for labels from the nodes data frame
+# returns a gTree with the labels
 #' @importFrom grid textGrob
 .get_labels <- function(nodes, fs=9, color = "black") {
   req_cols <- c("x", "y", "label")
@@ -157,18 +193,23 @@ adjust_fontsize <- function(x, path, font="fixed",
      x = "Missing required columns in nodes data frame: {.val {missing}}"))
   }
 
-  map(1:nrow(nodes), \(i) {
+  labels <- map(1:nrow(nodes), \(i) {
              textGrob(x = nodes$x[i], y = nodes$y[i],
                       label = nodes$label[i],
                       name = paste0("label_", nodes$node_key[i] %||% "NA"),
                       gp = gpar(col = nodes$color[i] %||% color,
                       fontsize = fs))
   })
+
+  labels <- gTree(gp = gpar(),
+                  children = do.call(gList, labels),
+                  name = "text")
+  labels
 }
 
 #' @importFrom grid rectGrob roundrectGrob
 #' @importFrom grid gpar unit
-.get_grob <- function(grobname, x, y,
+.get_rect <- function(grobname, x, y,
                       width, height,
                       name, col, fill, lwd=1) {
   if(grobname == "roundrectangle") {
@@ -176,7 +217,7 @@ adjust_fontsize <- function(x, path, font="fixed",
       name = name,
       width = width,
       height = height,
-      r = unit(.3, "snpc"),
+      r = unit(.15, "snpc"),
       gp = gpar(
                lwd = lwd,
                col = col,
@@ -198,6 +239,7 @@ adjust_fontsize <- function(x, path, font="fixed",
 }
 
 # create node grobs from the nodes data frame
+# returns a gTree with the rects
 #' @importFrom grid rectGrob roundrectGrob
 .get_node_rects <- function(nodes, lwd=1) {
   req_cols <- c("x", "y", "width", "height", "shape", "fill")
@@ -225,8 +267,8 @@ adjust_fontsize <- function(x, path, font="fixed",
     cli_abort(c(x = "NA values in the shape column"))
   }
 
-  map(1:nrow(nodes), \(i) {
-    .get_grob(grobname = nodes$shape[i],
+  rects <- map(1:nrow(nodes), \(i) {
+    .get_rect(grobname = nodes$shape[i],
               x = nodes$x[i],
               y = nodes$y[i],
               name = paste0("node_", nodes$node_key[i] %||% "NA"),
@@ -235,6 +277,14 @@ adjust_fontsize <- function(x, path, font="fixed",
               col = "black",
               fill = nodes$fill[i], lwd=lwd)
   })
+
+  if(!is.null(rects)) {
+    rects <- gTree(gp = gpar(),
+                   children = do.call(gList, rects),
+                   name = "rect")
+  }
+
+  rects
 }
 
 # create the arrows between the nodes
@@ -268,58 +318,103 @@ adjust_fontsize <- function(x, path, font="fixed",
 
   rects <- .get_node_rects(nodes, lwd = lwd)
 
-  if(!is.null(rects)) {
-    rects <- gTree(gp = gpar(),
-                   children = do.call(gList, rects),
-                   name = "rect")
-  }
-
   labels <- .get_labels(nodes, fs = fs)
-  labels <- gTree(gp = gpar(),
-                  children = do.call(gList, labels),
-                  name = "text")
 
   gTree(gp = gpar(),
         children = gList(rects=rects, labels=labels),
         name = name)
 }
 
-#' Hook for vtree plots
-#'
-#' This function is called whenever a vtree_plot is plotted on a device.
-#' It's purpose is to fit the
-#' labels text into the allocated node space.
-#'
-#' @param x A vtree_plot object
-#' @importFrom grid gTree gList setChildren makeContent
-#' @importFrom grid gPath
-#' @return A gTree object with the labels adjusted to fit into the allocated space.
-#' @export
-makeContent.vtree_plot <- function(x) {
-  spec <- x$params$spec_fontsize
+.make_legend <- function(legend, params) {
+  lwd       <- params$lwd
+  fontsizes <- params$fontsizes
 
-  for(i in seq_along(spec)) {
-    s <- spec[[i]]
-    x <- adjust_fontsize(x, s$path, font=s$fs, 
-                         padding = s$padding,
-                         widths = s$widths,
-                         heights = s$heights)
+  spec      <- list()
+  spec_lwd <- list()
+  kinder <- list()
+
+  # levels for layout_legend when legend=TRUE
+  if(!is.null(legend$levels)) {
+    kinder <- list(.get_nodes(legend$levels, name="levels", lwd = lwd))
+    spec$legend_levels <- list(path = c("legend", "levels", "text"),
+                               fs = fontsizes$legend_labels,
+                               widths = legend$levels$width,
+                               heights = legend$levels$height)
+    spec_lwd$legend_levels <- list(path = c("legend", "levels", "rect"))
   }
 
-  spec_lwd <- x$params$spec_lwd
-  lwd <- lwd_npc(0.001 * x$params$lwd)
-  for(i in seq_along(spec_lwd)) {
-    s <- spec_lwd[[i]]
-    x <- adjust_linewidth(x, s$path,
-                          lwd = x$params$lwd, s$nokids %||% FALSE)
-  }
+  # titles when legend=TRUE or var_labels != FALSE
+  kinder <- c(kinder,
+              list(.get_nodes(legend$titles, name="titles", lwd = lwd)))
 
-  x
+  spec$legend_titles <- list(path = c("legend", "titles", "text"),
+                             fs = fontsizes$var_labels,
+                             widths = legend$titles$width,
+                             heights = legend$titles$height,
+                             padding = .2)
+
+  legend <- gTree(gp=gpar(), children = do.call(gList, kinder),
+                  name = "legend")
+
+  list(ret = legend, spec = spec, spec_lwd = spec_lwd)
+}
+
+.make_grobs <- function(nodes, params) {
+
+  lwd       <- params$lwd
+  fontsizes <- params$fontsizes
+
+  spec <- list()
+  spec_lwd <- list()
+  kinder <- list()
+
+  nodes <- nodes |> mutate(shape = "rectangle")
+  rects <- .get_node_rects(nodes, lwd = lwd)
+
+  gnodes <- nodes |>
+    mutate(y = y - height/2 + height/20) |>
+    mutate(height = 3/4 * height) |>
+    mutate(width = 9/10 * width) |>
+    mutate(y = y + height / 2)
+
+  grobs <- map(seq_along(nodes$grob), \(i) {
+                 g <- gnodes$grob[[i]]
+                 g$vp <- grid::viewport(x = gnodes$x[i],
+                                        y = gnodes$y[i],
+                                        width = gnodes$width[i],
+                                        height = gnodes$height[i])
+                 g})
+  grobs <- gTree(gp = gpar(),
+                 children = do.call(gList, grobs),
+                 name = "plot_obj")
+
+  nodes$y <- nodes$y + nodes$height / 2
+  nodes$y <- nodes$y + (- 1/4 + 1/7.5) * nodes$height
+  nodes$height <- nodes$height / 8
+  labels <- .get_labels(nodes, fs = 9)
+
+  spec$plots <- list(path = c("plots", "text"),
+                             fs = "adaptive",
+                             widths = nodes$width,
+                             heights = nodes$height)
+
+
+# labels <- gTree(gp = gpar(),
+#                 children = do.call(gList, labels),
+#                 name = "text")
+#
+  ret <- gTree(gp = gpar(),
+        children = gList(rects=rects, grobs = grobs, text=labels),
+        name = "plots")
+
+
+  list(ret = ret, spec = spec, spec_lwd = spec_lwd)
 }
 
 # create the grobs associated with the plot. This is the main function that
 # actually creates the plot.
 #' @importFrom grid gTree gpar gList setChildren
+#' @importFrom purrr map_int map_lgl
 .make_children <- function(params, layout) {
   x <- gTree(params = params,
              layout = layout,
@@ -339,6 +434,24 @@ makeContent.vtree_plot <- function(x) {
   nodes <- as_tibble(layout)
   edges <- activate(layout, "edges") |> as_tibble()
 
+  children <- list()
+
+  if("grob" %in% colnames(nodes)) {
+    grobnodes <- map_lgl(nodes[["grob"]],
+                         \(g) {
+                           print(length(g))
+                           length(g) > 0L
+                         })
+    if(sum(grobnodes) > 0) {
+      message("found ", sum(grobnodes), " grobnodes")
+      gn <- .make_grobs(nodes[grobnodes, ], params)
+      children <- c(children, list(plots = gn$ret))
+
+      nodes <- nodes |> filter(!grobnodes)
+      spec <- c(spec, gn$spec)
+    }
+  }
+
   nodes_gt <- .get_nodes(nodes, fs = 9, lwd = lwd)
   arrows   <- .get_arrows(edges)
   spec_lwd$edges <- list(path = c("edges"), nokids = TRUE)
@@ -352,35 +465,17 @@ makeContent.vtree_plot <- function(x) {
   spec_lwd$nodes <- list(path = c("nodes", "rect"))
 
   # margin labels with the variable names
-  children <- gList(arrows=arrows, nodes=nodes_gt)
+  children <- c(children, list(arrows=arrows, nodes=nodes_gt))
 
   if(!is.null(legend)) {
-    kinder <- list()
+    ll <- .make_legend(legend, params)
+    spec <- c(spec, ll$spec)
+    spec_lwd <- c(spec, ll$spec_lwd)
 
-    # levels for layout_legend when legend=TRUE
-    if(!is.null(legend$levels)) {
-      kinder <- list(.get_nodes(legend$levels, name="levels", lwd = lwd))
-      spec$legend_levels <- list(path = c("legend", "levels", "text"),
-                                 fs = fontsizes$legend_labels,
-                                 widths = legend$levels$width,
-                                 heights = legend$levels$height)
-      spec_lwd$legend_levels <- list(path = c("legend", "levels", "rect"))
-    }
-
-    # titles when legend=TRUE or var_labels != FALSE
-    kinder <- c(kinder,
-                list(.get_nodes(legend$titles, name="titles", lwd = lwd)))
-
-    spec$legend_titles <- list(path = c("legend", "titles", "text"),
-                               fs = fontsizes$var_labels,
-                               widths = legend$titles$width,
-                               heights = legend$titles$height,
-                               padding = .2)
-
-    legend <- gTree(gp=gpar(), children = do.call(gList, kinder),
-                    name = "legend")
-    children <- gList(children, legend = legend)
+    children <- c(children, list(legend = ll$ret))
   }
+
+  children <- do.call(gList, children)
 
   x$params$spec_fontsize <- spec
   x$params$spec_lwd <- spec_lwd
