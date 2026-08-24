0378ef6ed7ed0f8522bc9e7d72437ee07844b494
January Weiner <january.weiner@gmail.com>
2026-08-24 08:31:06 +0200

    fixed incorrect positioning of legends and legend titles

--- R/legend.R
+++ R/legend.R
@@ -1,38 +1,57 @@
 # vertical legend arrangement
-.legend_vertical <- function(legend, titles, maxpos, margins) {
+.legend_vertical <- function(legend, titles, margins, title_size = 2) {
+
+  maxpos <- max(legend$nitems) + title_size
+  hg <- max(legend$full_h) / maxpos
+
+  pad <- .25 * hg
+
 
   legend <- legend |>
-    mutate(height = .data[["full_h"]] / maxpos) |>
     mutate(x = margins$left / 2) |>
     mutate(width = .8 * margins$left) |>
-    mutate(y = .data[["y"]] - .data[["full_h"]] / 2 +
-           (maxpos - .data[["pos"]] - 1.5) * .data[["height"]])
+    mutate(height = hg) |>
+    mutate(y = .data[["y"]] -
+           (.data[["nitems"]] + title_size) * hg / 2 +
+           (.data[["nitems"]] - .data[["pos"]]) * hg +
+           hg / 2 + pad)
 
   titles <- titles |>
-    mutate(height = 2 * .data[["full_h"]] / maxpos) |>
     mutate(x = margins$left / 2) |>
     mutate(width = .8 * margins$left) |>
-    mutate(y = .data[["y"]] + maxpos * .data[["height"]]/4 -
-           2*.data[["height"]]/3)
+    mutate(y = .data[["y"]] +
+           (.data[["nitems"]] + title_size) * hg / 2 -
+           hg +
+           pad) |>
+    mutate(height = title_size * hg - 2 * pad)
 
   list(levels=legend, titles=titles)
 }
 
 # horizontal legend arrangements
-.legend_horizontal <- function(legend, titles, maxpos, margins) {
+.legend_horizontal <- function(legend, titles, margins, title_size = 2) {
+
+  maxpos <- max(legend$nitems) + title_size
+  hg <- margins$bottom / maxpos
+
+  # pad goes: to the below the items, between items and title, and above
+  # the title. The necessary amount of space is removed from the title
+  # height, so 3 * pad must be smaller than hg * title_size.
+  pad <- 0.2 * hg
+
   legend <- legend |>
-    mutate(x = .data[["x"]],
-           y = margins$bottom -
-             (.data[["pos"]] + 1) * margins$bottom / maxpos) |>
-    mutate(height = .9 * margins$bottom / maxpos) |>
+    mutate(x = .data[["x"]]) |>
+    mutate(y = margins$bottom -
+             (.data[["pos"]] + title_size) * hg +
+             hg/2 + pad) |>
+    mutate(height = hg) |>
     mutate(width = .data[["width"]])
 
-  maxy <- max(legend[["y"]])
+  maxy <- max(legend[["y"]]) + hg / 2
 
   titles <- titles |>
-    mutate(height = .9 * margins$bottom / maxpos) |>
-    mutate(y = maxy + .data[["height"]] * 1.1) |>
-    mutate(height = .data[["height"]] * 2)
+    mutate(height = hg * title_size - 3 * pad) |>
+    mutate(y = maxy + .data[["height"]]/2 + pad)
 
   list(levels=legend, titles=titles)
 }
@@ -114,6 +133,8 @@ layout_legend <- function(layout, margins) {
   #req_cols <- c("x", "y", "width", "height", "shape", "fill", "label"))
   cnms <- names(layout)
 
+  # we get the positions of the variables from the layout, ignoring the
+  # root
   pospar <- as_tibble(layout) |>
     filter(.data[["node_key"]] != "node_1") |>
     distinct(pick("node_col"), .keep_all = TRUE) |>
@@ -140,9 +161,10 @@ layout_legend <- function(layout, margins) {
   legend <- merge(pospar, summaries, by = "node_col", all.y=TRUE) |>
       mutate(node_key = paste0("legend_", 1:n())) |>
       mutate(label_type = "var_level_label") |>
-      filter(!is.na(.data[["x"]]))
-
-  maxpos <- max(legend[["pos"]]) + 2
+      filter(!is.na(.data[["x"]])) |>
+      group_by(.data[["node_col"]]) |>
+      mutate(nitems = n()) |>
+      ungroup()
 
   titles <- legend |>
     group_by(.data[["node_col"]]) |>
@@ -162,9 +184,9 @@ layout_legend <- function(layout, margins) {
   }
 
   if(dir %in% c("tb", "bt")) {
-    legend <- .legend_vertical(legend, titles, maxpos, margins)
+    legend <- .legend_vertical(legend, titles, margins)
   } else {
-    legend <- .legend_horizontal(legend, titles, maxpos, margins)
+    legend <- .legend_horizontal(legend, titles, margins)
   }
 
   legend
@@ -194,12 +216,12 @@ layout_legend_minimal <- function(layout, margins) {
   }
 
   if(dir %in% c("bt", "tb")) {
-    nodes$width <- margins$left
+    nodes$width <- .8 * margins$left
     nodes$height <- nodes$full_h[1]
     nodes$x <- margins$left / 2
   } else {
-    nodes$width <- nodes$full_w[1]
-    nodes$height <- margins$bottom
+    nodes$width <- max(.6 * nodes$full_w[1], nodes$width[1])
+    nodes$height <- .8 * margins$bottom
     nodes$y <- margins$bottom / 2
   }
 
